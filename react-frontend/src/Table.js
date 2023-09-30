1d1456ff3cbb1536b677b321eeeaf07bcf771758
shua1090 <shynn.lawrence@gmail.com>
2023-09-30 15:24:24 -0700

    Updated MyApp.js/Table.js with state and deletion

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -5,6 +5,7 @@ function TableHeader() {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
+                <th>Remove People</th>
             </tr>
         </thead>
     );
@@ -16,6 +17,12 @@ function TableBody(props){
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={()=>
+                        props.removeCharacter(index)}>
+                            Delete
+                    </button>
+                </td>
             </tr>
         );
       }
@@ -31,7 +38,8 @@ function Table(props) {
     return (
         <table>
             <TableHeader />
-            <TableBody characterData={props.characterData}/>
+            <TableBody characterData={props.characterData}
+                removeCharacter={props.removeCharacter}/>
         </table>
     );
 }
