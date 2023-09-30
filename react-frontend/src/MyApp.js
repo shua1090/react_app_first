4c02a14e82344ef8fe3d883784b0cd6e691bffa8
shua1090 <shynn.lawrence@gmail.com>
2023-09-30 15:30:50 -0700

    Added form for adding people to table

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,28 +1,11 @@
 import React, {useState} from "react"
 import Table from "./Table"
-
+import Form from './Form'
 
 
 function MyApp() {
 
-    const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            name: "Mac",
-            job:"Bouncer"
-        },
-        {
-            name:"Dee",
-            job: "Aspiring actress"
-        },
-        {
-            name: "Dennis",
-            job: "Bartender"
-        }
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) =>{
@@ -36,6 +19,7 @@ function MyApp() {
         <div className="container">
             <Table characterData={characters}
                 removeCharacter={removeOneCharacter}/>
+            <Form />
         </div>
     );
 }
