681f114b1bcd3d7f46fc9b7a28980368c83bbb99
shua1090 <shynn.lawrence@gmail.com>
2023-10-03 09:02:39 -0700

    POST new users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,6 +41,11 @@ const findUserByName = (name) => {
 const findUserById = (id) => 
     users['users_list'].find( (user) => user['id'] === id);
 
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
@@ -66,6 +71,13 @@ app.get('/users/:id', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// Posting (Creating) User
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+})
 
 app.listen(port, ()=>{
     console.log(`Example app listening at http://localhost:${port}`);
