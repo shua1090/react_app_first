a7804c4c622ac0e1c4f5f187505617e4af15e529
shua1090 <shynn.lawrence@gmail.com>
2023-10-03 09:24:33 -0700

    User Deletion (REST Delete) and User job+name lookup

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,15 +41,42 @@ const findUserByName = (name) => {
 const findUserById = (id) => 
     users['users_list'].find( (user) => user['id'] === id);
 
+const findUserByIdAndJob = (name, job) => 
+    users['users_list'].find ( 
+        (user) => {
+            if (user['name'] === name && user['job'] == job)
+                return user;
+        }
+    );
+
 const addUser = (user) => {
     users['users_list'].push(user);
     return user;
 }
 
+const deleteUserById = (id) => {
+    const index = users['users_list'].findIndex( (user, ind) => {
+        if (user.id === id){
+            return ind;
+        }
+    });
+    if (index === undefined){
+        return undefined;
+    } else {
+        return users['users_list'].splice(index, 1);
+    }
+}
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    const job = req.query.job;
+
+    // To use double query, do: http.../users?name=Mac&job=Professor
+    if (name != undefined && job != undefined){
+        let result = findUserByIdAndJob(name, job);
+        result = {users_list: result};
+        res.send(result);
+    } else if (name != undefined){
         let result = findUserByName(name);
         result = {users_list: result};
         res.send(result);
@@ -61,6 +88,18 @@ app.get('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
     const id = req.params['id'];
     let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+})
+
+
+// Delete a user
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = deleteUserById(id);
     if (result === undefined) {
         res.status(404).send('Resource not found.');
     } else {
