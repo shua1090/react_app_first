788b79a41ac94ffbd94f60397818c3b74b9925c8
Shynn Lawrence <shynn.lawrence@gmail.com>
2023-10-07 14:11:58 -0700

    Changed frontend to display id, made delete column concise

--- packages/react-frontend/src/Table.js
+++ packages/react-frontend/src/Table.js
@@ -3,9 +3,10 @@ function TableHeader() {
     return (
         <thead>
             <tr>
+                <th>ID</th>
                 <th>Name</th>
                 <th>Job</th>
-                <th>Remove People</th>
+                <th>Remove</th>
             </tr>
         </thead>
     );
@@ -15,6 +16,7 @@ function TableBody(props){
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key={index}>
+                <td>{row.id}</td>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
                 <td>
