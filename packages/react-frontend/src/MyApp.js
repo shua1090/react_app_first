032a5577599bd758c30782eef06bc3bde8136ce9
Shynn Lawrence <shynn.lawrence@gmail.com>
2023-10-07 13:00:50 -0700

    Connected Frontend User Creation to Backend user creation (POST)

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -27,8 +27,21 @@ function MyApp() {
         setCharacters(updated);
     }
 
+    function postUser(person){
+        const promise = fetch("http://localhost:8000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person)
+        });
+        return promise;
+    }
+
     function updateList(person){
-        setCharacters([...characters, person]);
+        postUser(person)
+            .then( () => setCharacters([...characters, person]))
+            .catch( (error) => console.log(error) );
     }
 
     return (
