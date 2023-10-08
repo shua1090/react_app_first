1677e947ffadfe5f128e45e6e2c4a111371671e9
Shynn Lawrence <shynn.lawrence@gmail.com>
2023-10-07 18:22:11 -0700

    Removed hard-coded users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -9,30 +9,10 @@ app.use(express.json());
 const users = { 
     users_list : [
        { 
-          id : 'xyz789',
-          name : 'Charlie',
-          job: 'Janitor',
+          id : 1,
+          name : 'Ken Kubiak',
+          job: 'SE Professor',
        },
-       {
-          id : 'abc123', 
-          name: 'Mac',
-          job: 'Bouncer',
-       },
-       {
-          id : 'ppp222', 
-          name: 'Mac',
-          job: 'Professor',
-       }, 
-       {
-          id: 'yat999', 
-          name: 'Dee',
-          job: 'Aspring actress',
-       },
-       {
-          id: 'zap555', 
-          name: 'Dennis',
-          job: 'Bartender',
-       }
     ]
 };
 
